f7cf8367d6825306c74bbc7a0af91ec8f34265dd
Glitch (messenger-platform-quick-start) <none>
2024-03-05 11:01:38 +0000

    🌈🙈 Checkpoint ./app.js:65414559/4

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.post("/webhook", (req, res) => {
   let body = req.body;
 
   // Check the Incoming webhook message
-  console.log(JSON.stringify(req.body, null, 2));
+  console.log(JSON.stringify(body, null, 2));
  
   
 
